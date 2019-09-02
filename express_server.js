70ccb66eef9845e5a8d7511ce072a2e3cab10637
anthonykao10 <anthonykao10@gmail.com>
2019-09-02 17:02:09 +0000

    Add urls_show view, and setup route to render it with corresponding route params

--- express_server.js
+++ express_server.js
@@ -18,22 +18,17 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/:shortURL', (req, res) => {
+  let templateVars = { 
+    longURL: urlDatabase[req.params.shortURL],
+    shortURL: req.params.shortURL
+  };
+  res.render('urls_show', templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });  
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
