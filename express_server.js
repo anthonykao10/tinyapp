8dd695f5b6852733b7a1902bd695bcb88a6f9bb1
anthonykao10 <anthonykao10@gmail.com>
2019-09-03 21:30:41 +0000

    Add cookie-parser as a dependency

--- express_server.js
+++ express_server.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const app = express();
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const PORT = 8080;
 
+const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 // Return a string of 6 random alpha-numeric char's (only lowercase)
 function generateRandomString() {
