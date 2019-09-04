4a453d833e55e390a86669e98b988be0ebc89be4
anthonykao10 <anthonykao10@gmail.com>
2019-09-04 07:23:57 +0000

    Refactor auth routes into separate file

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,68 @@
 const router = require("express").Router();
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const db = require('../models/database');
+const util = require('../helpers');
+
+// LOGIN
+router.get('/login', (req, res) => {
+  const uid = req.cookies['user_id'];
+  let templateVars = {
+    user: db.users[uid]
+  }
+  res.render('login', templateVars);
+});
+
+router.post('/login', (req, res) => {
+  const uid = util.getUIDFromEmail(req.body.email);
+  // Verify user account exists
+  if (!uid) {
+    return res.status(400).send('Invalid email');
+  }
+  // Verify password is valid
+  if (db.users[uid].password !== req.body.password) {
+    return res.status(400).send('Invalid password');
+  }
+  res.cookie('user_id', uid);
+  res.redirect('/urls');
+});
+
+// LOGOUT
+router.post('/logout', (req, res) => {
+  res.clearCookie('user_id');
+  res.redirect('/urls');
+});
+
+// REGISTER
+router.get('/register', (req, res) => {
+  const uid = req.cookies['user_id'];
+  let templateVars = {
+    user: db.users[uid]  
+  }
+  res.render('register', templateVars);
+});
+
+router.post('/register', (req, res) => {
+  if (req.body.email === '' || req.body.password === '') {
+    return res.status(400).send('Email or Password cannot be empty');
+  }
+
+  if (util.getUIDFromEmail(req.body.email)) {
+    return res.status(400).send('Email already taken');
+  }
+
+  const uid = util.generateRandomString();
+  db.users[uid] = {
+    id: uid,
+    email: req.body.email,
+    password: req.body.password
+  };
+  console.log(db.users);
+  res.cookie('user_id', uid);
+  res.redirect('/urls');
+});
+
+// CATCH-ALL
+router.get('*', (req, res) => {
+  res.redirect('/urls');
+});
+
+module.exports = router;
