531dcf23704d88771181bbe88267e9a25ce83014
anthonykao10 <anthonykao10@gmail.com>
2019-09-04 06:55:40 +0000

    Refactor express_server by pulling urls and users "database" into separate models directory

---
+++ models/database.js
@@ -0,0 +1,22 @@
+exports.urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+exports.users = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  },
+  "test": {
+    id: "test", 
+    email: "test@gmail.com", 
+    password: "pass"
+  }
+};
