7bdfdfab923ac280f90167ba65729f9ba30acbca
anthonykao10 <anthonykao10@gmail.com>
2019-09-06 04:27:32 +0000

    Remove comments in database

--- database.js
+++ database.js
@@ -1,3 +1,4 @@
+// SAMPLE DATA FOR SCHEMA
 exports.urlDatabase = {
   "b2xVn2": {
     longURL: "http://www.lighthouselabs.ca",
@@ -26,24 +27,20 @@ exports.usersDatabase = {
     id: "userRandomID",
     email: "user@example.com",
     password: "$2b$10$Kwblw79bmixDFfJY3Zyvfu0ortF8fMlg1ESRJ.2zLAbo85smi56JO"
-    // password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
     id: "user2RandomID",
     email: "user2@example.com",
     password: "$2b$10$GRulM25c.Iv3LX5k3LCLHusqEM3L7Lnux5LpMu8nglPxaqlC7TOP."
-    // password: "dishwasher-funk"
   },
   "aJ48lW": {
     id: "aJ48lW",
     email: "test@test.com",
     password: "$2b$10$mQoRIccB9AMZYGBdby5U3uh9FBUtNdU8FU.MtU0e7a//RchqoYfJK"
-    // password: "pass"
   },
   "test": {
     id: "test",
     email: "test@gmail.com",
     password: "$2b$10$n9JfXAERfkvaf/4.yryoFO4iJaUUpsovwlp7aUNXr1TEi039nH3IC"
-    // password: "pass"
   }
 };
